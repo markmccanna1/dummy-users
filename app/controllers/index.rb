62b5c02b38c66694de7780b2e36ab8eec1545570
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:26:10 -0500

    creating controllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,44 @@
+
+#==================GET IT SUN=====================
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+
+
+
+
+get '/secret' do 
+  erb :secret
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#==================POST IT SUN=====================
+
+post '/validator' do
+  @user = User.find_by_user_name(user[:name])
+
+  if @user.check_pw(user[:password])
+    puts "GREAT SUCCESS"
+    redirect "/secret"
+  else
+    puts "GREAT FAILURE"
+    redirect "/"
+  end
+
 end
